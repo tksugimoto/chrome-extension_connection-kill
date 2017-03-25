c0addcc83e12e49f598a5229009b690d80eea95d
Takashi Sugimoto <takashi.sugimoto.git@gmail.com>
2017-03-25 22:02:36 +0900

    [Clean] function式 -> アロー関数

--- chrome-extension/background.js
+++ chrome-extension/background.js
@@ -1,4 +1,26 @@
 
+let blockFQDNList = {};
+const blockFullURLList = [];
+const REGEXP_FULL_URL = /^https?:[/][/]/;
+const REGEXP_SPACE_ONLY = /^[\s　]*$/;
+const updateBlockList = () => {
+	fetch("file/block-list.txt").then(response => {
+		return response.text();
+	}).then(responseText => {
+		blockFQDNList = {};
+		blockFullURLList.length = 0;
+		responseText.split(/\r*\n/).forEach(line => {
+			if (line.lastIndexOf("#", 0) === 0) return;
+			if (REGEXP_SPACE_ONLY.test(line)) return;
+			if (REGEXP_FULL_URL.test(line)) {
+				blockFullURLList.push(line);
+			} else {
+				blockFQDNList[line] = true;
+			}
+		});
+	});
+};
+
 chrome.browserAction.onClicked.addListener(updateBlockList);
 
 updateBlockList();
@@ -20,25 +42,3 @@ const opt_extraInfoSpec = [
 	"blocking"
 ];
 chrome.webRequest.onBeforeRequest.addListener(callback, filter, opt_extraInfoSpec);
-
-let blockFQDNList = {};
-const blockFullURLList = [];
-const REGEXP_FULL_URL = /^https?:[/][/]/;
-const REGEXP_SPACE_ONLY = /^[\s　]*$/;
-function updateBlockList() {
-	fetch("file/block-list.txt").then(response => {
-		return response.text();
-	}).then(responseText => {
-		blockFQDNList = {};
-		blockFullURLList.length = 0;
-		responseText.split(/\r*\n/).forEach(line => {
-			if (line.lastIndexOf("#", 0) === 0) return;
-			if (REGEXP_SPACE_ONLY.test(line)) return;
-			if (REGEXP_FULL_URL.test(line)) {
-				blockFullURLList.push(line);
-			} else {
-				blockFQDNList[line] = true;
-			}
-		});
-	});
-}
